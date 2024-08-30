939f00080c3edcfe57bb03de5df9623c5f4c0571
andreacagnina <amirkal@libero.it>
2024-08-30 13:51:32 +0200

    [feat] start carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,14 +2,22 @@
 export default {
     data() {
         return {
-
+            slides: [
+                '/src/assets/img/h5-slide-1-background.jpg',
+                '/src/assets/img/h5-slide-2-background.jpg',
+                '/src/assets/img/h5-slide-3-background.jpg',
+            ]
         }
     }
 }
 </script>
 
 <template>
-
+    <div class="carousel-container">
+        <div class="carousel-slide" v-for="(slide, index) in slides" :key="index">
+            <img :src="slide" alt="carousel image" />
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
