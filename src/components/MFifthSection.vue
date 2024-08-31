c6305e12eea9d587765262147fe9a670b858dd49
andreacagnina <amirkal@libero.it>
2024-08-31 16:35:05 +0200

    [feat] removed comp

--- src/components/MFifthSection.vue
+++ src/components/MFifthSection.vue
@@ -5,11 +5,22 @@ export default {
 </script>
 
 <template>
-    <div>
+    <div class="container bg-img">1
+        <div class="row">
+            <div class="col">
+                <div class="content">
 
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/Colors' as*;
+
+.bg-img {
+    background-image: url(../assets/img/page-background-img.png);
+    height: 100vh;
+}
 </style>
--- src/components/MThirdSection.vue
+++ src/components/MThirdSection.vue
@@ -1,16 +1,18 @@
 <script>
-import Avatar from './Avatar.vue';
+
 import { Testimonials } from '../data/Testimonials';
 
 
 export default {
-    components: {
-        Avatar,
-    },
     data() {
         return {
             Testimonials,
             currentIndex: 0,
+        }
+    },
+    methods: {
+        goToSlide(index) {
+            this.currentIndex = index;
         }
     },
 }
@@ -22,7 +24,17 @@ export default {
         <div class="row">
             <div class="col-12">
                 <div class="content position-absolute top-50 start-50 translate-middle text-center">
-                    <Avatar :Testimonials="Testimonials" />
+                    <div class="test-slider" :class="(index === currentIndex) ? 'active' : ''"
+                        v-for="(testimonial, index) in Testimonials" :key="index">
+                        <img :src="testimonial.url" alt="Avatar" class="avatar mb-3 d-none d-md-inline-block">
+                        <p class="my-5 lh-lg">{{ testimonial.cit }}</p>
+                        <h3>{{ testimonial.name }}</h3>
+                        <h5 class="text-uppercase my-3">{{ testimonial.role }}</h5>
+                    </div>
+                    <div class="slides-indicators my-5">
+                        <span v-for="(testimonial, index) in Testimonials" :key="index" @click="goToSlide(index)"><i
+                                :class="(index === currentIndex) ? 'fa-solid fa-circle active' : 'fa-solid fa-circle'"></i></span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -40,5 +52,40 @@ export default {
     background-size: cover;
     height: 100vh;
 
+}
+
+.avatar {
+    vertical-align: middle;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+.test-slider {
+    display: none;
+}
+
+.test-slider.active {
+    display: block;
+}
+
+
+span {
+    padding: 10px;
+
+    i {
+        cursor: pointer;
+        color: $ffffff4d;
+    }
+}
+
+.active {
+    color: $ffffff;
+}
+
+h2,
+h3 {
+    font-weight: 600;
 }
 </style>
