aa9a757bef378d258230ad6e40c79cbc5a3150ed
andreacagnina <amirkal@libero.it>
2024-09-01 12:26:06 +0200

    [feat] table

--- src/components/Main/MSixthSection.vue
+++ src/components/Main/MSixthSection.vue
@@ -2,32 +2,55 @@
 export default {
     data() {
         return {
-            cards: [
+            accounts: [
                 {
-                    url: 'src/assets/img/course-5-f-img.jpg',
-                    title: 'Android Developer',
-                    teacher: 'David Sanders',
-                    category: 'PROGRAMMING',
-                    numOfTeacher: '1',
-                    price: 'FREE',
-
+                    url: 'src/assets/img/h5-custom-icon-7.png',
+                    title: 'Standard',
+                    price: '$12',
+                    package: [
+                        {
+                            courses: '2',
+                            time: '15 Days',
+                            wd: true,
+                            hcd: true,
+                            marketing: false,
+                            python: false,
+                            android: false,
+                            english: false,
+                        }
+                    ]
                 },
                 {
-                    url: 'src/assets/img/course-6-f-img.jpg',
-                    title: 'Web Designing',
-                    teacher: 'Jennifer Powell',
-                    category: 'PROGRAMMING',
-                    numOfTeacher: '1',
-                    price: 'FREE',
-
+                    url: 'src/assets/img/h5-custom-icon-8.png',
+                    title: 'Professional',
+                    price: '$59',
+                    package: [
+                        {
+                            courses: '4',
+                            time: '30 Days',
+                            wd: true,
+                            hcd: true,
+                            marketing: true,
+                            python: true,
+                            android: false,
+                            english: false,
+                        }
+                    ]
                 },
                 {
-                    url: 'src/assets/img/course-12-f-img.jpg',
-                    title: 'Financial Modeling',
-                    teacher: 'Edward Bowman',
-                    category: 'BUSINESS',
-                    numOfTeacher: '1',
-                    price: '$20',
+                    url: 'src/assets/img/h5-custom-icon-9.png',
+                    title: 'Advanced',
+                    price: '$88',
+                    package: [{
+                        courses: '6',
+                        time: '30 Days',
+                        wd: true,
+                        hcd: true,
+                        marketing: true,
+                        python: true,
+                        android: true,
+                        english: true
+                    }]
                 },
             ],
             currentIndex: 0,
@@ -58,32 +81,68 @@ export default {
             <div class="row">
                 <div class="col-12">
                     <div class="content">
-                        <table class="table border">
+                        <table class="table table-bordered">
                             <thead>
                                 <tr>
                                     <th scope="col">Save up to 40% by paying weekly</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                    <th scope="col" v-for="account in accounts">
+                                        <div class="text-center">
+                                            <img :src="account.url" alt="">
+                                            <div>{{
+                                                account.title }}</div>
+                                            <div>{{ account.price }}</div>
+                                        </div>
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                                    <td scope="row">Number of Courses</td>
+                                    <td v-for="account in accounts">
+                                    <td v-for="a in account.package"> {{ a.courses }}</td>
+                                    </td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
+                                    <td scope="row">Time</td>
+                                    <td v-for="account in accounts">
+                                    <td v-for="a in account.package"> {{ a.time }}</td>
+                                    </td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
+                                    <td scope="row">Web Designing</td>
+                                    <td v-for="account in accounts">
+                                    <td v-for="a in account.package"> {{ a.wd }}</td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">Human-Centered Design</td>
+                                    <td v-for="account in accounts">
+                                    <td v-for="a in account.package"> {{ a.hcd }}</td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">Basic Marketing</td>
+                                    <td v-for="account in accounts">
+                                    <td v-for="a in account.package"> {{ a.marketing }}</td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">Python for Everybody</td>
+                                    <td v-for="account in accounts">
+                                    <td v-for="a in account.package"> {{ a.python }}</td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td scope="row"> Android Developer</td>
+                                    <td v-for="account in accounts">
+                                    <td v-for="a in account.package"> {{ a.android }}</td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">Business</td>
+                                    <td v-for="account in accounts">
+                                    <td v-for="a in account.package"> {{ a.english ? 'ok' : 'ko' }}</td>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
@@ -121,5 +180,9 @@ h6 {
 
 .active {
     color: $a40c4ff;
+}
+
+tr {
+    text-align: center;
 }
 </style>
